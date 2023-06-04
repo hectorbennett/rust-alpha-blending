60015d76a7bd5056461d96ed3c4d4b0761c6e012
Hector <hectorbennett@outlook.com>
2023-06-04 13:38:42 +0100

    step 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+pub mod step_1;
+pub mod step_2;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/step_1.rs
@@ -0,0 +1,14 @@
+type Rgba = [u8; 4];
+
+fn blend_rgba(bg: Rgba, fg: Rgba) -> Rgba {
+    unimplemented!();
+}
+
+#[test]
+fn test_blend_rgba() {
+    // test that layering translucent blue above opaque red results in opaque purple.
+    let opaque_red = [255, 0, 0, 255];
+    let translucent_blue = [0, 0, 255, 128];
+    let opaque_purple = [188, 0, 188, 255];
+    assert_eq!(blend_rgba(opaque_red, translucent_blue), opaque_purple);
+}
